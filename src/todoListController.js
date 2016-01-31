44f7910a2dcb793d83d563765a467ab36e810e84
hibreez <4ngus.whitehead@gmail.com>
2016-01-31 19:39:45 +0000

    Adds addTask function, passing feature test to add + display tasks.

--- src/todoListController.js
+++ src/todoListController.js
@@ -2,4 +2,8 @@ todoList.controller('TodoListController', [function() {
   var self = this;
   
   self.tasks = [];
+
+  self.addTask = function() {
+    self.tasks.push({ name: self.newTask })
+  }
 }]);
