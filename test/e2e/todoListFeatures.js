ca377b8e1832fe509409c29e6e6f90b73230edef
hibreez <4ngus.whitehead@gmail.com>
2016-02-01 00:11:14 +0000

    Adds feature test for task completion and creates test helper module.

--- test/e2e/todoListFeatures.js
+++ test/e2e/todoListFeatures.js
@@ -9,7 +9,7 @@ describe('TodoList', function() {
     pageHeader = element(by.className('page-header')).getText();
     newTaskEntry = element(by.model('todoCtrl.newTask'));
     addTaskButton = element(by.buttonText('Add Task'));
-    tasks = element.all(by.binding('task'));
+    tasks = element.all(by.repeater('task in todoCtrl.tasks'));
   });
 
   it('has a title tag and page header text', function() {
@@ -17,10 +17,30 @@ describe('TodoList', function() {
     expect(pageHeader).toEqual('to do:');
   });
 
-  it('stores and displays new tasks entered by the user', function() {
-    newTaskEntry.sendKeys('collect dry cleaning');
-    addTaskButton.click();
-    expect(tasks.first().getText()).toEqual('collect dry cleaning');
-    expect(helper.getInputValue(newTaskEntry)).toEqual('');
+  describe('adding tasks', function() {
+    it('stores and displays new tasks entered by the user', function() {
+      newTaskEntry.sendKeys('collect dry cleaning');
+      addTaskButton.click();
+      expect(tasks.first().getText()).toEqual('collect dry cleaning');
+      expect(helper.getInputValue(newTaskEntry)).toEqual('');
+    });
+  });
+
+  describe('marking completed tasks', function() {
+    beforeEach(function() {
+      newTaskEntry.sendKeys('collect dry cleaning');
+      addTaskButton.click();
+    });
+
+    it('tasks unmarked by default', function() {
+      expect(helper.isComplete(1)).toBeFalsy();
+      expect(helper.getTextClass(1)).toEqual('done-false');
+    });
+
+    it('strikes through task when checkbox checked', function() {
+      helper.completeTask(1);
+      expect(helper.isComplete(1)).toBeTruthy();
+      expect(helper.getTextClass(1)).toEqual('done-true');
+    });
   });
 });
--- test/e2e/todoListHelper.js
+++ test/e2e/todoListHelper.js
@@ -1,6 +1,36 @@
-exports.getInputValue = function(input) {
+function getInputValue(input) {
   return input.getAttribute('value')
     .then(function(value) {
-      return value
+      return value;
     });
-};
+}
+
+function getCheckBox(taskNumber) {
+  return element.all(by.model('task.done')).get(taskNumber - 1);
+}
+
+function isComplete(taskNumber) {
+  return getCheckBox(taskNumber).getAttribute('checked')
+    .then(function(value) {
+      return (value === 'true');
+    });
+}
+
+function completeTask(taskNumber) {
+  getCheckBox(taskNumber).click();
+}
+
+function getTextClass(taskNumber) {
+  return element.all(by.tagName('span')).get(taskNumber - 1)
+    .getAttribute('class')
+    .then(function(value) {
+      return value;
+    });
+}
+
+module.exports = {
+  getInputValue: getInputValue,
+  isComplete: isComplete,
+  completeTask: completeTask,
+  getTextClass: getTextClass
+}
