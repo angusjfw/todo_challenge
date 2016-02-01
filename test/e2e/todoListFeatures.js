59020a80ed087d231d4ec840f62a00502192a68a
hibreez <4ngus.whitehead@gmail.com>
2016-02-01 02:47:51 +0000

    Adds feature test for filtering tasks.

--- test/e2e/todoListFeatures.js
+++ test/e2e/todoListFeatures.js
@@ -2,6 +2,7 @@ var helper = require('./todoListHelper.js');
 
 describe('TodoList', function() {
   var title, pageHeader, newTaskEntry, addTaskButton, tasks;
+  var allTasksButton, activeTasksButton, completedTasksButton;
 
   beforeAll(function() {
     browser.get('http://localhost:5000');
@@ -10,6 +11,9 @@ describe('TodoList', function() {
     newTaskEntry = element(by.model('todoCtrl.newTask'));
     addTaskButton = element(by.buttonText('Add Task'));
     tasks = element.all(by.repeater('task in todoCtrl.tasks'));
+    allTasksButton = element(by.id('filter-all'));
+    activeTasksButton = element(by.id('filter-active'));
+    completedTasksButton = element(by.id('filter-completed'));
   });
 
   it('has a title tag and page header text', function() {
@@ -41,6 +45,32 @@ describe('TodoList', function() {
       helper.completeTask(1);
       expect(helper.isComplete(1)).toBeTruthy();
       expect(helper.getTextClass(1)).toEqual('done-true');
+    });
+  });
+
+  describe('filtering tasks', function() {
+    beforeEach(function() {
+      newTaskEntry.sendKeys('learn angular\n');
+      newTaskEntry.sendKeys('go shopping\n');
+      newTaskEntry.sendKeys('collect dry cleaning\n');
+      helper.completeTask(1);
+    });
+
+    it('can show all tasks', function() {
+      allTasksButton.click();
+      expect(tasks.length).toEqual(3);
+    });
+
+    it('can filter for active tasks', function() {
+      activeTasksButton.click();
+      expect(tasks.length).toEqual(2);
+      expect(tasks.first().name).toEqual('go shopping');
+    });
+
+    it('can filter for completed tasks', function() {
+      completedTasksButton.click();
+      expect(tasks.length).toEqual(1);
+      expect(tasks.first().name).toEqual('learn angular');
     });
   });
 });
